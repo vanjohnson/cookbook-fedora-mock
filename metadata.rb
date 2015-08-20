393d51b56fefb697602cc14eafaebf96a6286c3f
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-08-20 23:11:02 +0000

    pin dependency & add github urls

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,12 @@ description 'Installs Fedora Mock'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
+issues_url 'https://github.com/st-isidore-de-seville/cookbook-fedora-mock/issues'
+source_url 'https://github.com/st-isidore-de-seville/cookbook-fedora-mock'
+
 recipe 'fedora-mock::default', 'Installs Fedora Mock'
 
-depends 'yum-epel'
+depends 'yum-epel', '~> 0.6.2'
 
 supports 'redhat'
 supports 'centos'
