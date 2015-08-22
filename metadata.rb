116c93ad1ab9ce7e71d516b9eac5477f4706d51a
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-08-22 22:23:59 +0000

    fix #8

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description 'Installs Fedora Mock'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-issues_url 'https://github.com/st-isidore-de-seville/cookbook-fedora-mock/issues'
 source_url 'https://github.com/st-isidore-de-seville/cookbook-fedora-mock'
+issues_url 'https://github.com/st-isidore-de-seville/cookbook-fedora-mock/issues'
 
 recipe 'fedora-mock::default', 'Installs Fedora Mock'
 
